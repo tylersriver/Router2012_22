45552eef1591ffd9faaee8abd700e57feed1fae2
tyler.w.sriver <tyler.w.sriver@eagles.oc.edu>
2017-01-25 10:44:46 -0600

    Add Datagram header
    - Add Datagram interface
    - Add incomplete LL@P Frame Class

---
+++ app/src/main/java/sriver/w/tyler/router2017_22/networks/datagram_fields/DatagramHeaderField.java
@@ -0,0 +1,39 @@
+package sriver.w.tyler.router2017_22.networks.datagram_fields;
+
+/**
+ * Created by tyler.w.sriver on 1/24/17.
+ *
+ * This class is the generic interface
+ * for Datagram's used in the application
+ */
+public interface DatagramHeaderField {
+
+    // -- Methods
+    // --------------------------------------------------------------
+
+    /**
+     * Appropriate display string of field contents
+     * @return String
+     */
+    String toString();
+
+    /**
+     * Return contents as hex
+     * @return String
+     */
+    String toHexString();
+
+    /**
+     * Return string formatted to display
+     * content and meaning of field
+     * @return String
+     */
+    String explainSelf();
+
+    /**
+     * Return ASCII of hex conversion
+     * @return String
+     */
+    String toAsciiString();
+
+}
---
+++ app/src/main/java/sriver/w/tyler/router2017_22/networks/datagram_fields/LL2PAddressField.java
@@ -0,0 +1,40 @@
+package sriver.w.tyler.router2017_22.networks.datagram_fields;
+
+/**
+ * Created by tyler.w.sriver on 1/24/17.
+ *
+ * This class describes the structure of
+ * the LL2P Frame
+ */
+public class LL2PAddressField implements DatagramHeaderField {
+
+    // -- Fields
+    // --------------------------------------------------------------
+    private int address;
+    private boolean isSourceAddress;
+    private String explanation;
+
+    // -- Methods
+    // --------------------------------------------------------------
+
+
+    @Override
+    public String toString() {
+
+    }
+
+    @Override
+    public String toHexString() {
+
+    }
+
+    @Override
+    public String toAsciiString() {
+
+    }
+
+    @Override
+    public String explainSelf() {
+
+    }
+}
